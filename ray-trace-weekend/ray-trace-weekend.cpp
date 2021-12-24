e7e67719a65232b211161e03c6dfd5ee974d3eca
Azeem Bande-Ali <me@azeemba.com>
2021-12-24 16:43:34 -0500

    Commit template

---
+++ ray-trace-weekend/ray-trace-weekend.cpp
@@ -0,0 +1,11 @@
+﻿// ray-trace-weekend.cpp : Defines the entry point for the application.
+//
+
+#include "ray-trace-weekend.h"
+
+using namespace std;
+
+int main() {
+  cout << "Hello CMake." << endl;
+  return 0;
+}
