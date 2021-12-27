d01aeedbeac82602e83b74ea0af02024a6ff4963
Azeem Bande-Ali <me@azeemba.com>
2021-12-26 23:57:24 -0500

    Add brighter image + preview README

--- results/gen_previews.sh
+++ results/gen_previews.sh
@@ -1,3 +1,14 @@
 
 cd results
 find -name "*.ppm" | xargs -i{} magick convert {} {}.jpg
+
+echo "Previews" > README.md
+echo "=======" >> README.md
+echo "" >> README.md
+echo "" >> README.md
+
+for f in *.jpg
+do
+  echo "![$f]($f)" >> README.md
+  echo "" >> README.md
+done
